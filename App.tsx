1a2e697ad09ad794f2a363ac17313487c05455da
Nagender Kumar <nagenderkumar@Nagenders-MacBook-Pro.local>
2024-06-26 23:23:22 +0530

    Add search and list

--- App.tsx
+++ App.tsx
@@ -5,114 +5,139 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Button,
+  FlatList,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface Task {
+  key: string;
+  text: string;
+  completed: boolean;
 }
+const App = () => {
+  const [task, setTask] = useState<string>('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([
+        ...tasks,
+        {key: String(tasks.length + 1), text: task, completed: false},
+      ]);
+      setTask('');
+    }
   };
+  const toggleTask = (index: number) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+  const removeTask = (index: number) => {
+    const newTask = tasks.filter((_, i) => i !== index);
+    setTasks(newTask);
+  };
+  const filterTasks = searchQuery
+    ? tasks.filter(t => {
+        return t.text.toLowerCase().includes(searchQuery.toLowerCase());
+      })
+    : tasks;
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainers}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder=" Add new Task"
+            value={task}
+            onChangeText={setTask}></TextInput>
         </View>
-      </ScrollView>
+        <Button title="Add Task" onPress={addTask} />
+      </View>
+      <View style={styles.inputContainerSearch}>
+        <TextInput
+          style={styles.input}
+          placeholder=" Search Tasks"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+      </View>
+      <FlatList
+        data={filterTasks}
+        keyExtractor={task => task.key.toString()}
+        renderItem={({item, index}) => (
+          <View style={styles.taskItem}>
+            <Text
+              style={[
+                styles.taskText,
+                item.completed && styles.taskTextCompleted,
+              ]}
+              onPress={() => toggleTask(index)}>
+              {item.text}
+            </Text>
+            <TouchableOpacity onPress={() => removeTask(index)}>
+              <Text style={styles.removeTaskText}>Remove</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputContainers: {
+    flexDirection: 'row',
+    width: '100%',
+    backgroundColor: 'blue',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputContainerSearch: {
+    flexDirection: 'row',
+    width: '90%',
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    backgroundColor: 'white',
   },
-  highlight: {
-    fontWeight: '700',
+  inputContainer: {
+    flexDirection: 'row',
+    width: '60%',
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    backgroundColor: 'white',
+  },
+  taskItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  taskTextCompleted: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
+  },
+  removeTaskText: {
+    color: 'red',
   },
 });
-
 export default App;
